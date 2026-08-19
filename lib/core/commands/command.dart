dc9c650a89c848e7a4e7ed12f29aa110772e56ae
Van Nam <phamngocthuan@1123.com>
2026-08-19 10:42:05 +0700

    fix lỗi

--- lib/core/commands/command.dart
+++ lib/core/commands/command.dart
@@ -32,3 +32,45 @@ class Command0<T> extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+class Command1<T, P> extends ChangeNotifier {
+  final Future<Result<T>> Function(P) _action;
+  bool _running = false;
+  Result<T>? _result;
+
+  Command1(this._action);
+
+  bool get running => _running;
+  Result<T>? get result => _result;
+
+  Object? get error {
+    final res = _result;
+    if (res != null) {
+      return res.when(
+        ok: (_) => null,
+        error: (ex, msg) => msg,
+      );
+    }
+    return null;
+  }
+
+  Future<void> execute(P param) async {
+    if (_running) return;
+    _running = true;
+    _result = null;
+    notifyListeners();
+
+    try {
+      _result = await _action(param);
+    } catch (_) {
+    } finally {
+      _running = false;
+      notifyListeners();
+    }
+  }
+
+  void clearResult() {
+    _result = null;
+    notifyListeners();
+  }
+}
